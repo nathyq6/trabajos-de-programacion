4f4c07df475f2e2795b648c58768c54666b73c34
nathaly <nathalycuamatzi@gmail.com>
2024-04-29 10:29:50 -0600

    mi primer commit ejercicio6 version1 29/04/2mi primer commit ejercicio6 version1 29/04/2mi primer commit ejercicio6 version1 29/04/24ommitted:

---
+++ ejercicio6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+
+int carta;
+printf("dime el cumero de la carta");
+scanf("%d",&carta);
+
+if(carta==1){;
+printf("es un as\n");
+}
+else if(carta==10){;
+printf("es un sota\n");
+}
+ else if(carta==11){;
+printf("es un caballo\n");
+}
+else if(carta==12){;
+printf("es un rey\n");
+}
+else if(carta>=2 &&carta<=9){;
+printf("no es ninguna figura ni tampoco as");
+}
+else{;
+printf("este numero no esta en la baraja española\n");
+}
+
+return 0;
+}
